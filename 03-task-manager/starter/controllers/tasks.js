fd938102c0ec1560c02ba3691e557bb281ab759a
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-08-23 09:11:58 +0100

    handling or coding delete task in deleteTask controller

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -30,10 +30,12 @@ const getSingleTask = async (req, res) => {
     const task = await Task.findOne({_id:taskID})
     // if something went wrong with an id 
     if(!task){
+      // if we change an carachter in id we type 404 in status 
       return res.status(404).json({msg:`No task with id ${taskID}`})
     }
     res.status(200).json({task})
   } catch (error) {
+    // if id doesn't much the amount of carachters in status we type 500
     res.status(500).json({ msg: error });
   }
 };
@@ -41,8 +43,19 @@ const getSingleTask = async (req, res) => {
 const updateTask = (req, res) => {
   res.send("update task");
 };
-const deleteTask = (req, res) => {
-  res.send("delete task");
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id:taskID } = req.params
+    const task = await Task.findOneAndDelete({_id:taskID})
+    if(!task){
+      return res.status(404).json({msg:`No task with id ${taskID}`})
+    }
+    // we add task to json just for see task and see what are doing in postman 
+    res.status(200).json({ task })
+  } catch (error) {
+      res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
