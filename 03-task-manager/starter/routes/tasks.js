4bae5207f3d0f5d26601c80338834be1d5292bce
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-08-23 11:18:15 +0100

    add the different between put() and patch() in comment

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -15,5 +15,8 @@ const {
 router.route('/').get(getAllTasks).post(createTask)
 router.route('/:id').get(getSingleTask).patch(updateTask).delete(deleteTask)
 
+// the put() and the patch() methods are both for updating resource but: 
+// put() is: replace existing resource and rest of them effectively will be romved 
+// patch() is: for partial update ,we are just updating the properties that we're passing in 
 
 module.exports = router 
