022dffbe471c573d708b2bc3f4cc438080493c5f
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-08-27 14:20:06 +0100

    Sort - getAllProducts implementation

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -3,19 +3,21 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   // we chain sort() method if name normal that's mean a-z if negative z-a
   // if price normal that mean from smaller to bigger (<<<) if negative oposite (>>>>)
-  const products = await Product.find({}).sort('name -price');
+  const products = await Product.find({}).sort("name -price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   // console.log(req.query)
   // we handling if the property in data is there but the value doesn't much and if we try to setup a property doesn't exist in our data.
-  // we pull out only the properties names in our data
-  const { featured, company, name } = req.query;
-
+  // we pull out only the properties names in our query
+  const { featured, company, name, sort } = req.query;
+  console.log(sort);
+  // setup empty object
   const queryObejct = new Object();
   // handling featured
   if (featured) {
+    // add property to the object
     queryObejct.featured = featured == "true" ? true : false;
   }
   // handling company
@@ -26,8 +28,17 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObejct.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObejct);
-  const products = await Product.find(queryObejct);
+  // console.log(queryObejct);
+  let result = Product.find(queryObejct);
+  // handling sort 
+  if (sort) {
+      const sortList = sort.split(',').join(' ')
+      result = result.sort(sortList)
+  } else{
+    result = result.sort('')
+  }
+  // we add await after chaning find and sort methods in products variable and if sort false we await only find() method 
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
