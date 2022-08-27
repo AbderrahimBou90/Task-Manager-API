a17d1c7bee2b7d1d2e111edfb4d8f36353fb70d1
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-08-27 12:45:50 +0100

    Sort - general setup

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,9 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'abl'
-  const products = await Product.find({ 
-    name:{$regex:search,$options:'i'}
-   });
+  // we chain sort() method if name normal that's mean a-z if negative z-a
+  // if price normal that mean from smaller to bigger (<<<) if negative oposite (>>>>)
+  const products = await Product.find({}).sort('name -price');
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -12,19 +11,19 @@ const getAllProducts = async (req, res) => {
   // console.log(req.query)
   // we handling if the property in data is there but the value doesn't much and if we try to setup a property doesn't exist in our data.
   // we pull out only the properties names in our data
-  const { featured,company,name} = req.query;
+  const { featured, company, name } = req.query;
 
   const queryObejct = new Object();
   // handling featured
   if (featured) {
     queryObejct.featured = featured == "true" ? true : false;
   }
-  // handling company 
-  if(company){
-    queryObejct.company = company
+  // handling company
+  if (company) {
+    queryObejct.company = company;
   }
-  // handling name 
-  if(name){
+  // handling name
+  if (name) {
     queryObejct.name = { $regex: name, $options: "i" };
   }
   console.log(queryObejct);
