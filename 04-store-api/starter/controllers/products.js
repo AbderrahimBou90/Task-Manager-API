7efd565452f7b48c6a596de355e32c75070aa85f
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-08-27 11:31:43 +0100

    handling company property

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -7,14 +7,20 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   // console.log(req.query)
-  // we handling if the property in data is there but the value doesn't much and if we try to setup a property doesn't exist in our data 
-  const {featured} = req.query
+  // we handling if the property in data is there but the value doesn't much and if we try to setup a property doesn't exist in our data.
+  // we pull out only the properties names in our data
+  const { featured,company} = req.query;
 
-  const queryObejct = new Object()
-  if(featured){
-    queryObejct.featured = featured == 'true' ? true : false
+  const queryObejct = new Object();
+  // handling featured
+  if (featured) {
+    queryObejct.featured = featured == "true" ? true : false;
   }
-
+  // handling company 
+  if(company){
+    queryObejct.company = company
+  }
+  console.log(queryObejct);
   const products = await Product.find(queryObejct);
   res.status(200).json({ products, nbHits: products.length });
 };
