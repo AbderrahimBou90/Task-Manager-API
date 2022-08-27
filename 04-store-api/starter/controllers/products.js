3e067acf788b7cad8d487eb2557adf4a3e784817
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-08-27 09:57:22 +0100

    add automaticaly filter functionaltiy in find() method with Query Params

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,13 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const Products = await Product.find({ name: 'vase table' });
-  res.status(200).json({ Products, nbHits: Products.length });
+  const products = await Product.find({ name: "vase table" });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
