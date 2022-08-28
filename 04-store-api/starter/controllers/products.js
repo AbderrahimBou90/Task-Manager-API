3238d72c425da01cc4d79355f15732442b7b9032
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-08-28 11:28:52 +0100

    Pagination with skip and limit methods

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,9 +1,10 @@
 const Product = require("../models/product");
 
+// testing controller
 const getAllProductsStatic = async (req, res) => {
   // we chain sort() method if name normal that's mean a-z if negative z-a
   // if price normal that mean from smaller to bigger (<<<) if negative oposite (>>>>)
-  const products = await Product.find({}).select("name price").sort('name').limit(10).skip(1)
+  const products = await Product.find({}).select("name price").sort('name')
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -41,6 +42,18 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   } 
+  // handling pagination with limit and skip methods 
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page -1) * limit
+  // we have 23 items 
+  // we want 7 items in a page 
+  // we divide 23/7 we get 3.28 pages which means 3 pages each have 7 items and the last page have only 2 items (7 + 7 + 7 + 2 = 23) and with that we have 4 pages 
+  result = result.skip(skip).limit(limit)
+
+
+
+
   // we add await after chaning find and sort methods in products variable and if sort false we await only find() method 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
