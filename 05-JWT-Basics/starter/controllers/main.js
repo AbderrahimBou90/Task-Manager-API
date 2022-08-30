1aa18914ddbd53c6b19b2106d7783605b8bdd2f3
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-08-30 12:12:36 +0100

    Sign JWT

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -5,23 +5,33 @@
 // send back to front-end 
 
 // setup authontication so only the request with JWT can access the dashboard 
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 
 const login = async (req,res)=>{
- const {username, password} = req.body
- // we check username and password either by:
- // mongoose validation 
- // Joi package 
- // in our case check in controller
- if (!username || !password){
-  // we throw an error 
-  throw new CustomAPIError('Please provide email and password',400)
- }
+  const { username, password } = req.body;
+  // we check username and password either by:
+  // mongoose validation
+  // Joi package
+  // in our case check in controller
+  if (!username || !password) {
+    // we throw an error
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+
+  // just for demo, normally provided by DB!!!!
+  const id = new Date().getTime();
+
+  // in sign() method we need to provide 3 values: payload, a jwt secret, options
+  // try to keep payload small, better experience for user 
+  // just for Demo, in production we use JWT secret long, complex and unguessable string value !!!!!!! 
+  const token = jwt.sign({ id,username },process.env.JWT_SECRET,{expiresIn:'30d'});
 
 
- 
+  res.status(200).json({msg:'user created',token})
+}
+
 
 const dashboard = async (req,res) =>{
  const luckyNumber = Math.floor(Math.random() * 100)
