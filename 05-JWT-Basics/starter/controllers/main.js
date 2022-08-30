bbc4e6f78fe0920824f6b4ab3c22cc978f35071f
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-08-30 11:42:36 +0100

    Validate Username and Password

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,6 +1,27 @@
+// ––––––––––––––––– what about going to do ––––––––––––––––––
+// check username and password in post(login) request 
+// if both of them exist we create new JWT, if not we send back error respons
+// fi we create JWT then of course we want to send it back to front-end 
+// send back to front-end 
+
+// setup authontication so only the request with JWT can access the dashboard 
+// const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
+
+
 const login = async (req,res)=>{
- res.send('Fake Login/Register/Signup Route')
-}
+ const {username, password} = req.body
+ // we check username and password either by:
+ // mongoose validation 
+ // Joi package 
+ // in our case check in controller
+ if (!username || !password){
+  // we throw an error 
+  throw new CustomAPIError('Please provide email and password',400)
+ }
+
+
+ 
 
 const dashboard = async (req,res) =>{
  const luckyNumber = Math.floor(Math.random() * 100)
