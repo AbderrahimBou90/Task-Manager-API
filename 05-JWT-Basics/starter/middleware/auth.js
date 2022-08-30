9767ef8f7423a6f627abf6d2ec77d7939fba1483
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-08-30 14:20:52 +0100

    check auth Header middelware

---
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -0,0 +1,7 @@
+
+const authonticationMiddleware = async (req,res,next)=>{
+  console.log(req.headers.authorization)
+  next()
+}
+
+module.exports = authonticationMiddleware
--- 05-JWT-Basics/starter/routes/main.js
+++ 05-JWT-Basics/starter/routes/main.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const {login,dashboard} = require('../controllers/main')
+const authMiddleware = require('../middleware/auth')
 
-
-router.route('/dashboard').get(dashboard)
+// now every time someone hits this route first go through the middleware and since middleware has next() method then i'll pass to the dashboard 
+router.route('/dashboard').get(authMiddleware,dashboard)
 router.route('/login').post(login)
 
 module.exports = router
