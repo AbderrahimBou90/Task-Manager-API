5de81c8bc5a866b9cd780e6df97c09a8e0249ce1
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-09-01 10:23:52 +0100

    Auth Middeware Setup and Complite

--- 05-JWT-Basics/starter/middleware/auth.js
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -1,7 +1,30 @@
+// auth middleware (refactor code from dashboard controller)
 
-const authonticationMiddleware = async (req,res,next)=>{
-  console.log(req.headers.authorization)
-  next()
-}
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
 
-module.exports = authonticationMiddleware
+const authonticationMiddleware = async (req, res, next) => {
+  // console.log(req.headers)
+  const authHeader = req.headers.authorization;
+  // console.log(authHeader)
+  // check authHeader
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+  // extract only token
+  const token = authHeader.split(" ")[1];
+
+  // Verify Token
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(decoded)
+    const {id,username} = decoded
+    // setup user property with this object value
+    req.user = {id, username}
+    next();
+  } catch (error) {
+    throw new CustomAPIError("Not Authorized to access this route", 401);
+  }
+};
+
+module.exports = authonticationMiddleware;
