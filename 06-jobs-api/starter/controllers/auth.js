efeaba80e58553e119838db2a03dd8c54da061ba
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-09-02 18:12:20 +0100

    Error Checking - Controller Example

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,10 +1,17 @@
 // import user model
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const {BadRequestError} = require('../errors')
 
 const register = async (req, res) => {
+ // // we can check for empty values right in the controller
+ // const {name,email,password} = req.body
+ // if(!name || !email || !password){
+ //  throw new BadRequestError('Please provide name, email and password')
+ // }
+ // // but in this project we check for emptly values by mongoose validator 
+ // create user in DB
   const user = await User.create({ ...req.body });
-  console.log(user)
   res.status(StatusCodes.CREATED).json({ user });
 };
 
