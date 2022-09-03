a6e97fc3934bcd8d0122f6c33cd4dd1222d8fcc5
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-09-03 10:57:29 +0100

    Generate Token (JWT) in register controller

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,24 +1,28 @@
 // import user model
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const {BadRequestError} = require('../errors')
+const { BadRequestError } = require("../errors");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   // ••••••••••••••••••••• hashing password code •••••••••••••••••••••••••
-// // hashing is one-way street meaning cannot be reversed, really really good for stroing password 
-// // in order to generate hashed password we use two methods genStat() and hash() 
+  // // hashing is one-way street meaning cannot be reversed, really really good for stroing password
+  // // in order to generate hashed password we use two methods genStat() and hash()
 
-// const {name,email,password} = req.body
-// // genStal meaning generate random bytes, by providing number of rounds, keeping mind the more rounds you have the more processing power is going to require 
-// const salt = await bcrypt.genSalt(10)
-// // the hash() method looking for two things: the password that wanna hash and the salt 
-// const hashedPassword = await bcrypt.hash(password,salt)
-// const tempUserObj = {name,email,password:hashedPassword}
-// ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+  // const {name,email,password} = req.body
+  // // genStal meaning generate random bytes, by providing number of rounds, keeping mind the more rounds you have the more processing power is going to require
+  // const salt = await bcrypt.genSalt(10)
+  // // the hash() method looking for two things: the password that wanna hash and the salt
+  // const hashedPassword = await bcrypt.hash(password,salt)
+  // const tempUserObj = {name,email,password:hashedPassword}
+  // ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 
- // create user in DB
+  // create user in DB
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 
