df7545d01909ea67092b8fd4cab5f6ca2d2fa2d1
Abderrahim <abderrahim@MacBook-Pro-de-Abderrahim.local>
2022-09-07 09:36:21 +0100

    with customError we do not need CustomAPIError any more

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,4 +1,4 @@
-const { CustomAPIError } = require('../errors')
+// const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   
@@ -8,9 +8,9 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Something went wrong try again later'
   }
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
   if(err.code && err.code === 11000){
     // we overwriting msg and statusCode in customError object
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
